34d9a1e932152c43e070a64d0a89e75f48c1f781
Gurpreet Singh <cuzwhynotvlogs@gmail.com>
2021-04-12 17:28:27 -0400

    first commit

--- Xylophone/ViewController.swift
+++ Xylophone/ViewController.swift
@@ -1,21 +1,33 @@
-//
-//  ViewController.swift
-//  Xylophone
-//
-//  Created by Angela Yu on 28/06/2019.
-//  Copyright © 2019 The App Brewery. All rights reserved.
-//
-
 import UIKit
+import AVFoundation
 
 class ViewController: UIViewController {
+    
+    var player: AVAudioPlayer!
 
     override func viewDidLoad() {
         super.viewDidLoad()
     }
 
+    @IBAction func keyPressed(_ sender: UIButton) {
+        
+        playSound(currentTitle: sender.currentTitle!)
+        UIView.animate(withDuration: 0.3) { // makes transition smoother
+            sender.alpha = 0.5 // sets opacity to half
+        }
 
+        DispatchQueue.main.asyncAfter(deadline: .now() + 0.3 ) {
+            UIView.animate(withDuration: 0.3) {
+                sender.alpha = 1.0 // resets opacity
+            }
+        }
+    }
     
-
+    func playSound(currentTitle: String) {
+        let url = Bundle.main.url(forResource: currentTitle, withExtension: "wav")
+        player = try! AVAudioPlayer(contentsOf: url!)
+        player.play()
+                
+    }
 }
 
